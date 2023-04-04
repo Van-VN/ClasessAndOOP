231bdb8a5cda845f464ece1aa932343e334a56d3
Van Vu <vanvn.vu@gmail.com>
2023-04-04 10:25:44 +0700

    Added recipe object assignments

---
+++ objectbeginner.js
@@ -0,0 +1,41 @@
+let recipe = {
+  name: "Sườn xào chua ngọt",
+  serving: 2,
+  ingredients: [
+    "400g sườn thăn",
+    "4 tbsp nước mắm",
+    "5 tsp đường",
+    "5 tsp nước cốt chanh",
+    "1/3 bát nước sôi",
+    "Hành khô, tỏi, ớt, hành lá",
+    "2 quả cà chua",
+    "Hành tây (ớt chuông)",
+  ],
+};
+
+console.log(recipe);
+console.log("Tên món ăn: " + recipe.name);
+console.log("Khẩu phần ăn: " + recipe.serving);
+console.log("Thành phần: " + recipe.ingredients);
+
+// Tên món ăn: Sườn xào chua ngọt
+
+// Khẩu phần ăn: 2
+
+// Thành phần:
+
+// - 400 g sườn thăn
+
+// - 4 tbsp nước mắm
+
+// - 5 tsp đường
+
+// - 5 tsp nước cốt chanh
+
+// - 1/3 bát nước sôi
+
+// - Hành khô, tỏi, ớt, hành lá
+
+// - 2 quả cà chua
+
+// - Hành tây (ớt chuông)
